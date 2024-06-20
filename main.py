5b2f7886c60e58b5e9e5292d146ff87f5a814c83
maximiliangeyer <maximiliangeyer1@gmail.com>
2024-06-20 18:54:32 +0200

    example

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from MainWindow import MainWindow
+
+app = QApplication(sys.argv)
+
+main_window = MainWindow()
+main_window.show()
+
+sys.exit(app.exec())
